7cae1324580bf1c3befe81e0f33fd47e6fe05e8d
Md. Abdul Awal Sazib <sazib1207@gmail.com>
2022-04-22 05:03:32 +0600

    Allure report issue fixed

--- testcases/test_calculator_functions_for_addition.py
+++ testcases/test_calculator_functions_for_addition.py
@@ -3,16 +3,13 @@ import pytest
 from app.calculator_app import Calculator
 
 
-@allure.testcase
 def test_addition_work_for_negative_numbers():
     assert Calculator.add(-103, -23) == -126
 
 
-@allure.testcase
 def test_addition_work_for_positive_numbers():
     assert Calculator.add(103, 23) == 126
 
 
-@allure.testcase
 def test_addition_work_for_positive_negative_numbers():
     assert Calculator.add(103, -23) == 80
--- testcases/test_calculator_functions_for_subtraction.py
+++ testcases/test_calculator_functions_for_subtraction.py
@@ -4,18 +4,18 @@ from app.calculator_app import Calculator
 
 
 @allure.step('adding two negative numbers should produce negative result')
-@allure.severity_level(allure.severity_level.CRITICAL)
+@allure.severity(allure.severity_level.CRITICAL)
 def test_subtraction_work_for_negative_numbers():
     assert Calculator.subtract(-103, -23) == -80
 
 
 @allure.step('adding two positive numbers should produce positive result')
-@allure.severity_level(allure.severity_level.CRITICAL)
+@allure.severity(allure.severity_level.CRITICAL)
 def test_addition_work_for_positive_numbers():
     assert Calculator.subtract(103, 23) == 80
 
 
 @allure.step('adding a positive number with a negative number')
-@allure.severity_level(allure.severity_level.MINOR)
+@allure.severity(allure.severity_level.MINOR)
 def test_addition_work_for_positive_negative_numbers():
     assert Calculator.subtract(103, -23) == 126
