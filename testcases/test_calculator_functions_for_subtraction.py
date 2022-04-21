6eed6a6d65ac5559b443a16e9b2d40120d391f0a
Md. Abdul Awal Sazib <sazib1207@gmail.com>
2022-04-22 04:23:35 +0600

    Allure reporting added

--- testcases/test_calculator_functions_for_subtraction.py
+++ testcases/test_calculator_functions_for_subtraction.py
@@ -3,16 +3,19 @@ import pytest
 from app.calculator_app import Calculator
 
 
-@allure.testcase
+@allure.step('adding two negative numbers should produce negative result')
+@allure.severity_level(allure.severity_level.CRITICAL)
 def test_subtraction_work_for_negative_numbers():
     assert Calculator.subtract(-103, -23) == -80
 
 
-@allure.testcase
+@allure.step('adding two positive numbers should produce positive result')
+@allure.severity_level(allure.severity_level.CRITICAL)
 def test_addition_work_for_positive_numbers():
     assert Calculator.subtract(103, 23) == 80
 
 
-@allure.testcase
+@allure.step('adding a positive number with a negative number')
+@allure.severity_level(allure.severity_level.MINOR)
 def test_addition_work_for_positive_negative_numbers():
     assert Calculator.subtract(103, -23) == 126
