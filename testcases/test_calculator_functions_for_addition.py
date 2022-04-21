8886eb30064f24d3d039f921cc94901ff5e66e04
Md. Abdul Awal Sazib <sazib1207@gmail.com>
2022-04-22 02:48:16 +0600

    Initial commit.

---
+++ testcases/test_calculator_functions_for_addition.py
@@ -0,0 +1,14 @@
+import pytest
+from app.calculator_app import Calculator
+
+
+def test_addition_work_for_negative_numbers():
+    assert Calculator.add(-103, -23) == -126
+
+
+def test_addition_work_for_positive_numbers():
+    assert Calculator.add(103, 23) == 126
+
+
+def test_addition_work_for_positive_negative_numbers():
+    assert Calculator.add(103, -23) == 80
