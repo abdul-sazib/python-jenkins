8265feed0694e6e72900c85862ebfda4ad7d12bd
Md. Abdul Awal Sazib <sazib1207@gmail.com>
2022-04-22 04:10:03 +0600

    Allure report integrated

--- testcases/test_calculator_functions_for_addition.py
+++ testcases/test_calculator_functions_for_addition.py
@@ -1,14 +1,18 @@
+import allure
 import pytest
 from app.calculator_app import Calculator
 
 
+@allure.testcase
 def test_addition_work_for_negative_numbers():
     assert Calculator.add(-103, -23) == -126
 
 
+@allure.testcase
 def test_addition_work_for_positive_numbers():
     assert Calculator.add(103, 23) == 126
 
 
+@allure.testcase
 def test_addition_work_for_positive_negative_numbers():
     assert Calculator.add(103, -23) == 80
--- testcases/test_calculator_functions_for_subtraction.py
+++ testcases/test_calculator_functions_for_subtraction.py
@@ -1,14 +1,18 @@
+import allure
 import pytest
 from app.calculator_app import Calculator
 
 
+@allure.testcase
 def test_subtraction_work_for_negative_numbers():
     assert Calculator.subtract(-103, -23) == -80
 
 
+@allure.testcase
 def test_addition_work_for_positive_numbers():
     assert Calculator.subtract(103, 23) == 80
 
 
+@allure.testcase
 def test_addition_work_for_positive_negative_numbers():
     assert Calculator.subtract(103, -23) == 126
